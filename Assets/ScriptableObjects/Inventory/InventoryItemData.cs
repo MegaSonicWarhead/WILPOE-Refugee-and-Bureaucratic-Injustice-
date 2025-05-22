cce276e5de8eb9045a5cc763830a9cbdde4931ae
Lilycorpzz <143013925+Lilycorpzz@users.noreply.github.com>
2025-05-23 00:52:03 +0200

    Update InventoryItemData.cs

--- Assets/ScriptableObjects/Inventory/InventoryItemData.cs
+++ Assets/ScriptableObjects/Inventory/InventoryItemData.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
-public class InventoryItemData : MonoBehaviour
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class InventoryItemData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string itemName;
+    public Sprite icon;
+    public int maxStackSize = 99;
 }
