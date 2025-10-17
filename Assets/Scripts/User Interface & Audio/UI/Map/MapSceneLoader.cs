feda71b6dd29d7bd4f220de7435274839d685b43
MegaSonicWarhead <markperkins98@gmail.com>
2025-10-17 12:53:09 +0200

    Update

--- Assets/Scripts/User Interface & Audio/UI/Map/MapSceneLoader.cs
+++ Assets/Scripts/User Interface & Audio/UI/Map/MapSceneLoader.cs
@@ -7,35 +7,35 @@ public class MapSceneLoader : MonoBehaviour
 {
     private void Awake()
     {
-        Debug.Log($"[MapSceneLoader] Awake in scene: {SceneManager.GetActiveScene().name}, object: {gameObject.name}");
+       //Debug.Log($"[MapSceneLoader] Awake in scene: {SceneManager.GetActiveScene().name}, object: {gameObject.name}");
     }
 
     private void OnEnable()
     {
-        Debug.Log($"[MapSceneLoader] OnEnable called for {gameObject.name}");
+       // Debug.Log($"[MapSceneLoader] OnEnable called for {gameObject.name}");
     }
 
     private void Start()
     {
-        Debug.Log($"[MapSceneLoader] Start called in scene: {SceneManager.GetActiveScene().name}");
+        //Debug.Log($"[MapSceneLoader] Start called in scene: {SceneManager.GetActiveScene().name}");
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            Debug.Log($"[MapSceneLoader] 'M' pressed in scene {SceneManager.GetActiveScene().name}. Attempting to load 'Map'");
+            //Debug.Log($"[MapSceneLoader] 'M' pressed in scene {SceneManager.GetActiveScene().name}. Attempting to load 'Map'");
             SceneManager.LoadScene("Map"); // Make sure your map scene is actually called "Map"
         }
     }
 
     private void OnDisable()
     {
-        Debug.Log($"[MapSceneLoader] OnDisable called for {gameObject.name}");
+        //Debug.Log($"[MapSceneLoader] OnDisable called for {gameObject.name}");
     }
 
     private void OnDestroy()
     {
-        Debug.LogError($"[MapSceneLoader] OnDestroy called for {gameObject.name} in scene: {SceneManager.GetActiveScene().name}");
+        //Debug.LogError($"[MapSceneLoader] OnDestroy called for {gameObject.name} in scene: {SceneManager.GetActiveScene().name}");
     }
 }
