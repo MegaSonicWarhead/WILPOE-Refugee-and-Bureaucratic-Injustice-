de6ca352513130ff8b7e2613cd2aaae16aafcb11
Lilycorpzz <143013925+Lilycorpzz@users.noreply.github.com>
2025-11-23 00:32:44 +0200

    Itemdata changes

--- Assets/Scripts/InventoryItemData.cs
+++ Assets/Scripts/InventoryItemData.cs
@@ -1,5 +1,11 @@
 
 using UnityEngine;
+public enum ItemType
+{
+    Ingredient,
+    Meal
+}
+
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class InventoryItemData : ScriptableObject
@@ -7,7 +13,13 @@ public class InventoryItemData : ScriptableObject
     public string itemName;
     public Sprite icon;
     public int maxStackSize = 99;
+    public ItemType itemType;
+
 
     [Header("Shop")]
     public int price;
+
+    [Header("World Prefab (for Ingredients or Meals)")]
+    public GameObject worldPrefab;
+
 }
