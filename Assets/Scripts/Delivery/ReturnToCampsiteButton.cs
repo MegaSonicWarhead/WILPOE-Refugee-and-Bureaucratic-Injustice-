c582fd09a87f982bf0589d7640b997694ea0e8a6
Tapuwa Kabwato <141030379+tapskabs@users.noreply.github.com>
2025-11-22 13:50:02 +0200

    Button To Job

--- Assets/Scripts/Delivery/ReturnToCampsiteButton.cs
+++ Assets/Scripts/Delivery/ReturnToCampsiteButton.cs
@@ -4,11 +4,17 @@ using UnityEngine.SceneManagement;
 public class ReturnToCampsiteButton : MonoBehaviour
 {
     [Header("Scene Settings")]
-    public string Campsite = "Campsite"; 
+    public string Campsite = "Campsite";
+    public string DeliveryGame = "DeliveryGame";
 
     public void ReturnToCampsite()
     {
         Debug.Log("Returning to campsite scene: " + Campsite);
         SceneManager.LoadScene(Campsite);
+    }
+    public void ToJob()
+    {
+        Debug.Log("Returning to campsite scene: " + DeliveryGame);
+        SceneManager.LoadScene(DeliveryGame);
     }
 }
